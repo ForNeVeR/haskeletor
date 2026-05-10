376254542ad2b703d17621109f9b37c5756fb584
Friedrich von Never <friedrich@fornever.me>
2026-05-11 00:04:22 +0200

    (#57) HaskellStackConfigurationBase: untie from the IntelliJ modules

--- src/main/scala/me/fornever/haskeletor/runconfig/HaskellStackConfigurationBase.scala
+++ src/main/scala/me/fornever/haskeletor/runconfig/HaskellStackConfigurationBase.scala
@@ -8,23 +8,16 @@
 
 package me.fornever.haskeletor.runconfig
 
-import com.intellij.execution.configurations.{ConfigurationFactory, ModuleBasedConfiguration, RunConfigurationModule, RuntimeConfigurationException}
-import com.intellij.openapi.module.{Module, ModuleManager}
+import com.intellij.execution.configurations.{ConfigurationFactory, LocatableConfigurationBase}
 import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.XmlSerializer
 import org.jdom.Element
 
-import scala.jdk.CollectionConverters._
-
 abstract class HaskellStackConfigurationBase(name: String, project: Project, configurationFactory: ConfigurationFactory)
-  extends ModuleBasedConfiguration[RunConfigurationModule, HaskellStackConfigurationBase](name, new RunConfigurationModule(project), configurationFactory) {
+  extends LocatableConfigurationBase[HaskellStackConfigurationBase](project, configurationFactory, name) {
 
   private var workingDirPath: String = ""
   private var stackArgs: String = ""
-
-  ModuleManager.getInstance(getProject).getModules.toList.headOption.foreach(m => setModule(m))
-
-  override def getValidModules: java.util.Collection[Module] = ModuleManager.getInstance(getProject).getModules.toList.asJava
 
   override def writeExternal(element: Element): Unit = {
     super.writeExternal(element)
@@ -34,12 +27,6 @@ abstract class HaskellStackConfigurationBase(name: String, project: Project, con
   override def readExternal(element: Element): Unit = {
     super.readExternal(element)
     XmlSerializer.deserializeInto(this, element)
-  }
-
-  override def checkConfiguration(): Unit = {
-    val selectedModule = getConfigurationModule.getModule
-    if (selectedModule == null)
-      throw new RuntimeConfigurationException("Haskell module is not selected")
   }
 
   def setWorkingDirPath(workingDirPath: String): Unit = {
