4d7fd2edc3b3b3dd6ae815390eb9dc1351148afd
Friedrich von Never <friedrich@fornever.me>
2026-05-10 18:52:42 +0200

    (#57) HaskellProjectManager: initial VFS scan

--- projectModel/src/main/kotlin/me/fornever/haskeletor/projectmodel/HaskellProjectManager.kt
+++ projectModel/src/main/kotlin/me/fornever/haskeletor/projectmodel/HaskellProjectManager.kt
@@ -6,18 +6,27 @@
 
 package me.fornever.haskeletor.projectmodel
 
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.diagnostic.trace
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootEvent
+import com.intellij.openapi.roots.ModuleRootListener
 import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.newvfs.BulkFileListenerBackgroundable
 import com.intellij.openapi.vfs.newvfs.events.*
 import com.jetbrains.rd.util.reactive.IOptPropertyView
 import com.jetbrains.rd.util.reactive.OptProperty
+import com.jetbrains.rd.util.reactive.hasValue
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.launch
 import me.fornever.haskeletor.core.intellij.ProjectScope
 import java.nio.file.Path
 import kotlin.io.path.Path
@@ -50,8 +59,58 @@ class HaskellProjectManager(private val project: Project) {
                 VirtualFileManager.VFS_CHANGES_BG,
                 createVfsListener()
             )
+            subscribe(ModuleRootListener.TOPIC, object : ModuleRootListener {
+                override fun rootsChanged(event: ModuleRootEvent) {
+                    progressModuleRootChange()
+                }
+            })
         }
+        launchInitialCheck()
+    }
 
+    private fun launchInitialCheck() {
+        ProjectScope.get(project).launch(Dispatchers.IO) {
+            readAction {
+                val roots = ProjectRootManager.getInstance(project).contentRootsFromAllModules
+                logger.info("Started scanning ${roots.size} project content roots.")
+                val time = System.currentTimeMillis()
+                try {
+                    for (root in roots) {
+                        coroutineContext.ensureActive()
+                        findConfigFiles(root).forEach { stackFile ->
+                            processConfigFile(stackFile, exists = true)
+                        }
+                    }
+                } catch (e: CancellationException) {
+                    logger.info("Full scan of VFS cancelled. Wasted ${System.currentTimeMillis() - time} ms.")
+                    throw e
+                } finally {
+                    logger.info("Finished scanning ${roots.size} project content roots in ${System.currentTimeMillis() - time} ms.")
+                }
+            }
+        }
+    }
+
+    private fun findConfigFiles(directory: VirtualFile): List<VirtualFile> {
+        val result = mutableListOf<VirtualFile>()
+        if (!directory.isDirectory) return result
+
+        val queue = ArrayDeque<VirtualFile>()
+        queue.add(directory)
+
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+            if (!current.isDirectory) continue
+
+            for (child in current.children) {
+                if (child.isDirectory) {
+                    queue.add(child)
+                } else if (child.name.equals("stack.yaml", ignoreCase = true)) {
+                    result.add(child)
+                }
+            }
+        }
+        return result
     }
 
     @Suppress("UnstableApiUsage")
@@ -87,14 +146,35 @@ class HaskellProjectManager(private val project: Project) {
     private fun processConfigFile(file: VirtualFile, exists: Boolean) {
         if (!isConfigFile(file.toNioPath())) return
 
-        val fileProjectIndex = ProjectFileIndex.getInstance(project)
-        val shouldExist = exists && fileProjectIndex.isInContent(file) // TODO: Subscribe to the ProjectFileIndex's content root changes somehow
+        val projectFileIndex = ProjectFileIndex.getInstance(project)
+        val shouldExist = exists && projectFileIndex.isInContent(file)
         logger.trace {
             "Processing file \"${file.path}\"." +
-                " Exists: $exists, isInContent: ${fileProjectIndex.isInContent(file)}."
+                " Exists: $exists, isInContent: ${projectFileIndex.isInContent(file)}."
         }
 
         modifyConfigFileMap(file.toNioPath(), shouldExist)
+    }
+
+    private fun progressModuleRootChange() {
+        // TODO: In theory, it's possible that a new module root is added without VFS modification — should be handled somehow.
+        val fileProjectIndex = ProjectFileIndex.getInstance(project)
+
+        synchronized(_configFilesLock) {
+            for (path in _configFiles) {
+                val file = VirtualFileManager.getInstance().findFileByNioPath(path) ?: run {
+                    logger.error("Cannot find file \"${path.pathString}\" in VFS.")
+                    continue
+                }
+
+                if (!fileProjectIndex.isInContent(file)) {
+                    _configFiles.remove(path)
+                    logger.info("Unregistered a config file due to module root change: \"${path.pathString}\".")
+                }
+            }
+
+            _isHaskellProject.set(_configFiles.isNotEmpty())
+        }
     }
 
     private fun modifyConfigFileMap(path: Path, shouldExist: Boolean) {
@@ -111,7 +191,7 @@ class HaskellProjectManager(private val project: Project) {
                 }
             }
 
-            if (changed) {
+            if (!_isHaskellProject.hasValue || changed) {
                 _isHaskellProject.set(_configFiles.isNotEmpty())
             }
         }
